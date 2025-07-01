024ae2bd56b6e9051fee392ecc03403abdd77888
Mayank Mathur <139607998+mrmayankmathur@users.noreply.github.com>
2025-07-01 14:33:42 +0530

    app.py

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ genai.configure(api_key=os.getenv("GOOGLE_API_KEY"))
 
 # Function to load Gemini model and get responses
 def get_gemini_response(image, prompt):
-    model = genai.GenerativeModel('gemini-1.5-pro')
+    model = genai.GenerativeModel('gemini-2.5-pro')
     response = model.generate_content([image[0], prompt])
     return response.text
 
